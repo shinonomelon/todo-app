8f609aa36afcfc00d25d3c532c78b77929d47a00
shinonomelon <70379917+shinonomelon@users.noreply.github.com>
2024-04-29 16:51:19 +0900

    ヘッダー改善

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,32 @@
-export function Header() {
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/libs/supabase/server";
+
+export async function Header() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const signOut = async () => {
+    "use server";
+
+    const supabase = createClient();
+    await supabase.auth.signOut();
+    return redirect("/login");
+  };
+
   return (
     <header className="mx-auto flex max-w-3xl justify-end p-4 font-semibold text-emerald-600">
-      <button>login</button>
+      {user ? (
+        <form action={signOut}>
+          <button className="rounded-md px-4 py-2 no-underline hover:bg-gray-200">Logout</button>
+        </form>
+      ) : (
+        <Link href="/login">Login</Link>
+      )}
     </header>
   );
 }
