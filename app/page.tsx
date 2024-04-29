ed7bc86eeb8c4b8603a4455e71a5e4b1ef039824
shinonomelon <70379917+shinonomelon@users.noreply.github.com>
2024-04-29 13:11:04 +0900

    TodoListコンポーネントに分ける

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ import { TodoList } from "@/components/todo-list";
 
 export default async function Home() {
   return (
-    <main className="mx-auto flex min-h-screen max-w-3xl flex-col items-center p-24">
+    <main className="mx-auto flex min-h-screen max-w-3xl flex-col items-center px-24 py-8">
       <h1 className="text-4xl font-bold">Todo App</h1>
       <CreateTodoForm />
       <div className="w-full">
-        <Suspense fallback={<div>loading...</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
           <TodoList />
         </Suspense>
       </div>
