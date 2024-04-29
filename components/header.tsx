2ad3408b0ab4dfd8cf395c2e8bdbbde29cef124d
shinonomelon <70379917+shinonomelon@users.noreply.github.com>
2024-04-29 13:10:09 +0900

    ヘッダー追加

---
+++ components/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="mx-auto flex max-w-3xl justify-end p-4 font-semibold text-emerald-600">
+      <button>login</button>
+    </header>
+  );
+}
